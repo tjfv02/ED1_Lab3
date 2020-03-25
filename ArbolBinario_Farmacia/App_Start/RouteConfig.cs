e3767974c351e3844733612b40c8204ce7f7e587
Mansi1108 <djmoralesmonzon@gmail.com>
2020-03-25 14:24:33 -0600

    RouteConfig arreglado

--- ArbolBinario_Farmacia/App_Start/RouteConfig.cs
+++ ArbolBinario_Farmacia/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ArbolBinario_Farmacia
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Arbol", action = "PaginaPrincipal", id = UrlParameter.Optional }
             );
         }
     }
